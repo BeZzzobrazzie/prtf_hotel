7986cc543289689734674b33af8b4d9a7a847fc6
BeZzzobrazzie <61871773+BeZzzobrazzie@users.noreply.github.com>
2023-02-05 19:56:10 +0300

    working on masked text field

--- src/blocks/masked-text-field/masked-text-field.js
+++ src/blocks/masked-text-field/masked-text-field.js
@@ -3,7 +3,8 @@ class MaskedTextField {
     this.domElement = domElement;
     this.initEventListener(this.domElement);
     console.log('init textField');
-    this.previous = domElement.value;
+    this.previous = '';
+    this.reg = /^(?:(?:31(\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
   }
 
   initEventListener(domElement) {
@@ -26,14 +27,107 @@ class MaskedTextField {
         }
         break;
       case 'input':
-        //console.log(this.previous);
-        for(let i of event.target.value) {
-          console.log(i);
-          if(!i.match(/\d/)) {
-            console.log('error');
+
+        let regexp1 = /[0-3]/;
+        let regexp2 = /0[1-9]|[12]\d|3[01]/;
+        let regexp4 = /(0[1-9]|[12]\d|3[01])\.[01]/;
+        let regexp5 = /(0[1-9]|[12]\d|3[01])\.(0[1-9])|(1[012])/; 
+        let regexp10 = /(0[1-9]|[12]\d|3[01])\.(0[1-9])|(1[012])\.(\d\d\d[1-9])/; // нужно сделать проверку на 0000 и 0001. !!Сейчас неверно
+
+        let formatDate = event.target.value.replace(/\./g, '');
+        console.log('formatDate:' + formatDate);
+        console.log('this.previous.length:' + this.previous.length);
+        console.log('event.target.value.length:' + event.target.value.length);
+        if(this.previous.length < event.target.value.length) { // увеличение
+          if(event.target.value.length > 10) {
             event.target.value = this.previous;
           }
+          else {
+            switch(event.target.value.length) {
+              case '0':
+                break;
+              case 1:
+                if(!regexp1.test(event.target.value)) {
+                  event.target.value = this.previous;
+                }
+                break;
+              case 2:
+                if(!regexp2.test(event.target.value)) {
+                  event.target.value = this.previous;
+                }
+                else {
+                  event.target.value += '.';
+                }
+                break;
+              case 4:
+                if(!regexp4.test(event.target.value)) {
+                  event.target.value = this.previous;
+                }
+                break;
+              case 5:
+                if(!regexp5.test(event.target.value)) {
+                  event.target.value = this.previous;
+                }
+                else {
+                  event.target.value += '.';
+                }
+                break;
+              case 10:
+                if(!regexp10.test(event.target.value)) {
+                  event.target.value = this.previous;
+                }
+                break;
+            }
+          }
+          
+
+          // if(event.target.value.length == 2 || event.target.value.length == 5) {
+          //   event.target.value += '.';
+          // }
         }
+        else if(this.previous.length > event.target.value.length) { // уменьшение
+          if(event.target.value.length == 2) {
+            event.target.value = event.target.value.slice(0, 1);
+          }
+          else if(event.target.value.length == 5) {
+            event.target.value = event.target.value.slice(0, 4);
+          }
+        }
+
+        //console.log(event.target.value.length);
+        // switch(formatDate.length) {
+        //   case '0':
+        //     break;
+        //   case 1:
+        //     if(!regexp1.test(formatDate)) {
+        //       event.target.value = this.previous;
+        //     }
+        //     break;
+        //   case 2:
+        //     if(!regexp2.test(formatDate)) {
+        //       event.target.value = this.previous;
+        //     }
+        //     else {
+        //       event.target.value += '.';
+        //     }
+        //     break;
+        // }
+
+        //console.log(this.previous);
+        // for(let i of event.target.value) {
+        //   //console.log(i);
+        //   if(!i.match(/\d/)) {
+        //     console.log('error');
+        //     event.target.value = this.previous;
+        //   }
+        // }
+
+        // if(regexp.test(event.target.value)) {
+        //   console.log('ok');
+        // }
+        // else {
+        //   console.log('not ok');
+        // }
 
 
         this.previous = event.target.value;
