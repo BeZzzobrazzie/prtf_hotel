5a6b6f6c616da3eb03b9058dc8f93887c144db88
BeZzzobrazzie <61871773+BeZzzobrazzie@users.noreply.github.com>
2023-02-12 18:30:59 +0300

    ~-

--- src/blocks/masked-text-field/masked-text-field.js
+++ src/blocks/masked-text-field/masked-text-field.js
@@ -2,32 +2,27 @@ class MaskedTextField {
   constructor(domElement) {
     this.domElement = domElement;
     this.initEventListener(this.domElement);
-    console.log('init textField');
     this.previous = '';
     this.reg = /^(?:(?:31(\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+    this.dateObj = {
+      day: '',
+      month: '',
+      year: '',
+      dotFirst: false,
+      dotSecond: false,
+    }
   }
 
   initEventListener(domElement) {
     domElement.addEventListener('focusin', this);
     domElement.addEventListener('focusout', this);
     domElement.addEventListener('input', this);
-    domElement.addEventListener('click', this);
-    domElement.addEventListener('select', this);
-
-
+    domElement.addEventListener('keydown', this);
   }
 
 
   handleEvent(event) {
     switch (event.type) {
-      case 'select':
-        console.log('hi');
-
-        break;
-      case 'click':
-        console.log(event.target.selectionStart);
-
-        break;
       case 'focusin':
         if (event.target.value == event.target.getAttribute('value')) {
           event.target.value = '';
@@ -40,91 +35,84 @@ class MaskedTextField {
         break;
       case 'input':
 
-        let validChar = /[^\d-\/\.]/;
-        if (validChar.test(event.target.value)) {  // проверка на недопустимые символы
-          event.target.value = this.previous;
-          this.errAlert();
+        if(!this.checkInput(event)) {
           break;
         }
+        
 
-        let lengthNewStr = event.target.value.length - this.previous.length;
-        let newStr = event.target.value.substr(this.previous.length, lengthNewStr);
-        //console.log('Пользователь ввёл: ' + newStr);
-
-        if (event.target.value.length > 10) {    // проверка на максимальное количество символов (скорее всего избыточно)
-          event.target.value = this.previous;
-          this.errAlert();
-          break;
-        }
-
-        if (Array.from(event.target.value.matchAll(/\.|\/|-/g)).length > 2) {  // проверка на количество разделителей
-          event.target.value = this.previous;
-          this.errAlert();
-          break;
-        }
-
-        if (Array.from(event.target.value.matchAll(/\d/g)).length > 8) {       // проверка на количество символов
-          event.target.value = this.previous;
-          this.errAlert();
-          break;
-        }
-
-        console.log(event.target.value.split(/\.|\/|-/));
-        console.log(this.previous);
+        //console.log(event.target.value.split(/\.|\/|-/));
+        //console.log(this.previous);
 
         let splitDate = event.target.value.split(/\.|\/|-/);
-        let day = splitDate[0];
-        let month = splitDate[1];
-        let year = splitDate[2];
+        // let day = splitDate[0];
+        // let month = splitDate[1];
+        // let year = splitDate[2];
+
+        this.dateObj.day = splitDate[0];
+        this.dateObj.month = splitDate[1];
+        this.dateObj.year = splitDate[2];
+        if (Array.from(event.target.value.matchAll(/\.|\/|-/g)).length == 1) {
+          this.dateObj.dotFirst = true;
+        }
+        else if (Array.from(event.target.value.matchAll(/\.|\/|-/g)).length == 2) {
+          this.dateObj.dotSecond = true;
+        }
 
 
-        if (day != undefined && day.length > 2) {
+        if (this.dateObj.day != undefined && this.dateObj.day.length > 2) {
           event.target.value = this.previous;
           this.errAlert();
           break;
         }
 
-        if (month != undefined && month.length > 2) {
+        if (this.dateObj.month != undefined && this.dateObj.month.length > 2) {
           event.target.value = this.previous;
           this.errAlert();
           break;
         }
 
-        if (year != undefined && year.length > 4) {
+        if (this.dateObj.year != undefined && this.dateObj.year.length > 4) {
           event.target.value = this.previous;
           this.errAlert();
           break;
         }
 
+        if (this.dateObj.dotFirst && this.dateObj.dotSecond) {
 
+        }
+        else if (this.dateObj.dotFirst && !this.dateObj.dotSecond) {
 
-        // if (day.length == 2 && event.target.value[2] != '.') {
-        //   event.target.value += '.';
-        // }
+        }
+        else if (!this.dateObj.dotFirst && !this.dateObj.dotSecond) {
+          
+        }
+
 
 
         if (this.previous.length <= event.target.value.length) { // увеличение
-          if (Array.from(event.target.value.matchAll(/\.|\/|-/g)).length == 0 && day.length == 2) {
-            event.target.value += '.';
-          }
-          if (Array.from(event.target.value.matchAll(/\.|\/|-/g)).length == 1 && day.length == 1) {
-            event.target.value = '0' + event.target.value;
-          }
-          if (Array.from(event.target.value.matchAll(/\.|\/|-/g)).length == 1 && month != undefined && month.length == 2) {
-            event.target.value += '.';
-          }
-          // if (Array.from(event.target.value.matchAll(/\.|\/|-/g)).length == 2 && month.length == 1) {
-          //   event.target.value = event.target.value.slice(0, 3) + '0' + event.target.value.slice(3);
+          // if (Array.from(event.target.value.matchAll(/\.|\/|-/g)).length == 0 && this.dateObj.day.length == 2) {
+          //   event.target.value += '.';
+          // }
+          // if (Array.from(event.target.value.matchAll(/\.|\/|-/g)).length == 1 && this.dateObj.day.length == 1) {
+          //   event.target.value = '0' + event.target.value;
+          // }
+          // if (Array.from(event.target.value.matchAll(/\.|\/|-/g)).length == 1 && this.dateObj.month != undefined && this.dateObj.month.length == 2) {
+          //   event.target.value += '.';
           // }
           
-          if (Array.from(event.target.value.matchAll(/\.|\/|-/g)).length == 2) {
-            if(day.length == 1) {
-              event.target.value = '0' + event.target.value;
-            }
-            if(month.length == 1) {
-              event.target.value = event.target.value.slice(0, 3) + '0' + event.target.value.slice(3);
-            }
-          }
+          // if (Array.from(event.target.value.matchAll(/\.|\/|-/g)).length == 2) {
+          //   if(this.dateObj.day.length == 1) {
+          //     event.target.value = '0' + event.target.value;
+          //   }
+          //   if(this.dateObj.month.length == 1) {
+          //     event.target.value = event.target.value.slice(0, 3) + '0' + event.target.value.slice(3);
+          //   }
+          // }
+
+
+
+
+
         }
         else if (this.previous.length == event.target.value.length) { // изменение при том же количестве символов
 
@@ -133,8 +121,21 @@ class MaskedTextField {
 
         }
 
-        console.log('hi');
         this.previous = event.target.value;
+        break;
+
+      case 'keydown':
+        if (
+          event.code == 'ArrowLeft' || 
+          event.code == 'ArrowRight' || 
+          event.code == 'ArrowUp' || 
+          event.code == 'ArrowDown' || 
+          event.code == 'ArrowUp ctrlKey' || 
+          event.code == 'ArrowDown ctrlKey' || 
+          event.code == 'Home' || 
+          event.code == 'End') {          
+          this.getCursorPosition(this.domElement.querySelector('.masked-text-field__input'));
+        }
         break;
     }
   }
@@ -145,6 +146,41 @@ class MaskedTextField {
     setTimeout(() => {
       elem.style.borderColor = '';
     }, 1000);
+  }
+
+
+  getCursorPosition(parent) {
+    setTimeout(() => {
+      console.log(parent.selectionStart);
+    }, 1)
+  }
+
+  checkInput(event) {
+    let validChar = /[^\d-\/\.]/;
+    if (validChar.test(event.target.value)) {  // проверка на недопустимые символы
+      event.target.value = this.previous;
+      this.errAlert();
+      return false;
+    }
+
+    if (event.target.value.length > 10) {    // проверка на максимальное количество символов (скорее всего избыточно)
+      event.target.value = this.previous;
+      this.errAlert();
+      return false;
+    }
+
+    if (Array.from(event.target.value.matchAll(/\.|\/|-/g)).length > 2) {  // проверка на количество разделителей
+      event.target.value = this.previous;
+      this.errAlert();
+      return false;
+    }
+
+    if (Array.from(event.target.value.matchAll(/\d/g)).length > 8) {       // проверка на количество символов
+      event.target.value = this.previous;
+      this.errAlert();
+      return false;
+    }
+    return true;
   }
   
 }
